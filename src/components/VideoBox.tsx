939e1f4446e01ca77d836d6180b2b580bdabfc7d
janekitpk <janekitpk1@gmail.com>
2025-07-18 16:51:17 +0700

    fix videoBox : change video url

--- src/components/VideoBox.tsx
+++ src/components/VideoBox.tsx
@@ -4,7 +4,7 @@ const VideoBox = (props: Props) => {
   return (
     <div className="flex flex-col absolute z-[-1] top-0 w-full">
       <video className="w-full" autoPlay loop muted playsInline>
-        <source src="/assets/videos/Sequence.mp4" type="video/mp4" />
+        <source src="/videos/Sequence.mp4" type="video/mp4" />
       </video>
       <div className="w-full h-screen bg-[#81ac74]" />
     </div>
